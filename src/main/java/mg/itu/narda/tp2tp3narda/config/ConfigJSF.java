ce25d2d151167378c8f0272bd4663c9a659774fe
Narda-github <nardaarandria@gmail.com>
2024-04-06 10:13:47 +0300

    Création  du projet pour TP2 et TP3

---
+++ src/main/java/mg/itu/narda/tp2tp3narda/config/ConfigJSF.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package mg.itu.narda.tp2tp3narda.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author Narda <mg.itu.narda>
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+
+}
